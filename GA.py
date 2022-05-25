16e0428f5fd2ea017b2d6592f844deb2dd3f96e9
钱泽昊 <1936879727@qq.com>
2022-05-25 13:14:30 +0800

    Update GA.py

--- GA.py
+++ GA.py
@@ -1,53 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Find the global maximum for function: f(x) = x + 10sin(5x) + 7cos(4x)
+'''
+
 from math import sin, cos
-# 导入种群和内置算子相关类
+
 from gaft import GAEngine
-from gaft.components import GAIndividual
-from gaft.components import GAPopulation
-from gaft.operators import RouletteWheelSelection
+from gaft.components import BinaryIndividual
+from gaft.components import Population
+from gaft.operators import TournamentSelection
 from gaft.operators import UniformCrossover
 from gaft.operators import FlipBitMutation
-# 用于编写分析插件的接口类
+
+# Analysis plugin base class.
 from gaft.plugin_interfaces.analysis import OnTheFlyAnalysis
-# 内置的存档适应度函数的分析类
-from gaft.analysis.fitness_store import FitnessStoreAnalysis
-# 我们将用两种方式将分析插件注册到遗传算法引擎中
 
+# Built-in best fitness analysis.
+from gaft.analysis.fitness_store import FitnessStore
 
-# 定义种群
-indv_template = GAIndividual(ranges=[(0, 10)], encoding='binary', eps=0.001)
-population = GAPopulation(indv_template=indv_template, size=50)
-# 创建遗传算子
-selection = RouletteWheelSelection()
+# Define population.
+indv_template = BinaryIndividual(ranges=[(0, 10)], eps=0.001)
+population = Population(indv_template=indv_template, size=30).init()
+
+# Create genetic operators.
+selection = TournamentSelection()
 crossover = UniformCrossover(pc=0.8, pe=0.5)
 mutation = FlipBitMutation(pm=0.1)
-# 创建遗传算法引擎, 分析插件和适应度函数可以以参数的形式传入引擎中
+
+# Create genetic algorithm engine.
 engine = GAEngine(population=population, selection=selection,
                   crossover=crossover, mutation=mutation,
-                  analysis=[FitnessStoreAnalysis])
+                  analysis=[FitnessStore])
+
+# Define fitness function.
 
 
 @engine.fitness_register
 def fitness(indv):
-    x, = indv.variants
+    x, = indv.solution
     return x + 10*sin(5*x) + 7*cos(4*x)
+
+# Define on-the-fly analysis.
 
 
 @engine.analysis_register
 class ConsoleOutputAnalysis(OnTheFlyAnalysis):
     interval = 1
+    master_only = True
 
-    def register_step(self, ng, population, engine):
+    def register_step(self, g, population, engine):
         best_indv = population.best_indv(engine.fitness)
-        msg = 'Generation: {}, best fitness: {:.3f}'.format(
-            ng, engine.fitness(best_indv))
-        engine.logger.info(msg)
+        msg = 'Generation: {}, best fitness: {:.3f}'.format(g, engine.ori_fmax)
+        self.logger.info(msg)
 
     def finalize(self, population, engine):
         best_indv = population.best_indv(engine.fitness)
-        x = best_indv.variants
-        y = engine.fitness(best_indv)
+        x = best_indv.solution
+        y = engine.ori_fmax
         msg = 'Optimal solution: ({}, {})'.format(x, y)
-        engine.logger.info(msg)
+        self.logger.info(msg)
 
 
 if '__main__' == __name__:
